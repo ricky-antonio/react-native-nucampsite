a108fde369ebdd16f7feb5028a9efe6a86859c3d
Ricardo Monterrosa <rickyantonio117@gmail.com>
2021-04-09 14:46:36 -0700

    Navigation Part 2

---
+++ components/HomeComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    };
+
+    static navigationOptions = {
+        title: 'Home'
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Home Component</Text>
+            </View>
+        );
+    };
+};
+
+export default Home;
