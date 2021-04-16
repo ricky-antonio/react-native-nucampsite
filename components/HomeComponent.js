5730cafaba61e38d52b1614c3cc2e26236e0199a
Ricardo Monterrosa <rickyantonio117@gmail.com>
2021-04-16 16:05:42 -0700

    Redux Adding Favorites

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -15,16 +15,19 @@ const mapStateToProps = state => {
 
 function RenderItem(props) {
     const { item } = props;
+    console.log(props)
 
     if (props.isLoading) {
         return <Loading />;
     };
 
     if (props.errMess) {
-        <View>
-            <Text>{props.errMess}</Text>
-        </View>
-    }
+        return (
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    };
 
     if (item) {
         return (
@@ -55,15 +58,15 @@ class Home extends Component {
                 <RenderItem
                     item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
                     isLoading={this.props.campsites.isLoading}
-                    errMess={this.props.campsites.errMess} 
+                    errMess={this.props.campsites.errMess}
                 />
                 <RenderItem
-                    item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]} 
+                    item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
                     isLoading={this.props.promotions.isLoading}
                     errMess={this.props.promotions.errMess}
                 />
                 <RenderItem
-                    item={this.props.partners.partners.filter(partner => partner.featured)[0]} 
+                    item={this.props.partners.partners.filter(partner => partner.featured)[0]}
                     isLoading={this.props.partners.isLoading}
                     errMess={this.props.partners.errMess}
                 />
